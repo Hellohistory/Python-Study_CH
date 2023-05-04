0b3700a1ad4da28829cc8cffe30c3f492d60e640
Hellohistory <85825011+Hellohistory@users.noreply.github.com>
2023-05-04 19:01:16 +0800

    微调

--- 混沌系统演示/相图_ Logistic 映射.py
+++ 混沌系统演示/相图_ Logistic 映射.py
@@ -7,6 +7,7 @@ def main():
         try:
             x0 = float((input("请输入初始 x 值(0到1之间):")))
             y0 = float((input("请输入初始 y 值(0到1之间):")))
+            num =int(input('请输入您想要迭代的次数:'))
             if 0 < x0 and x0 < 1 and 0 < y0 and y0 < 1:
                 break
             else:
@@ -14,7 +15,7 @@ def main():
         except Exception as e:
             print("请输入正确的数字。")
 
-    print("接下来，程序将进行1000次迭代，并绘制相图:")
+    print(f"接下来，程序将进行{num}次迭代，并绘制相图:")
 
     x = x0
     y = y0
@@ -22,11 +23,11 @@ def main():
     xs = []
     ys = []
 
-    for i in range(1000):
+    for i in range(num):
         xs.append(x)
         ys.append(y)
-        x, y = 3.9 * x * (1 - x), 3.9 * y * (1 - y) # 混沌系统的计算公式
-        print(f"第{i+1}次迭代的结果: x={x}, y={y}")
+        x, y = 3.9 * x * (1 - x), 3.9 * y * (1 - y)  # 混沌系统的计算公式
+        print(f"第{i + 1}次迭代的结果: x={x}, y={y}")
 
     plt.scatter(xs, ys, s=0.1, c='black')
     plt.xlabel('x')
