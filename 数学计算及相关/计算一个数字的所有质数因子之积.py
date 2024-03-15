cdb2be4806332ad130f552df025f449133de8dd2
Hellohistory <etojsyc521@gmail.com>
2024-03-15 23:58:35 +0800

    20240315更新

--- 数学计算及相关/计算一个数字的所有质数因子之积.py
+++ 数学计算及相关/计算一个数字的所有质数因子之积.py
@@ -1,16 +1,17 @@
 def productPrimeFactors(n):
     product = 1
-    for i in range(2, n+1):
-        if (n % i == 0):
+    for i in range(2, n + 1):
+        if n % i == 0:
             isPrime = 1
-            for j in range(2, int(i/2 + 1)):
-                if (i % j == 0):
+            for j in range(2, int(i / 2 + 1)):
+                if i % j == 0:
                     isPrime = 0
                     break
             # 如果 'i' 是质数并且是num的因子
-            if (isPrime):
+            if isPrime:
                 product = product * i
     return product
+
 
 n = int(input('请输入数字:'))
 print(productPrimeFactors(n))
